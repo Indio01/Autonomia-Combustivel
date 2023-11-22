7d831f5b6b2d039cd4bee123fb65c25afe667122
Nodd <124269320+Indio01@users.noreply.github.com>
2023-11-22 13:38:13 +0000

    combustivel

--- App.js
+++ App.js
@@ -1,40 +1,94 @@
-import { StatusBar } from 'expo-status-bar';
-import {useState} from 'react';
-import { StyleSheet, Text, View, TextInput, Pressable} from 'react-native';
-import {Picker} from '@react-native-picker/picker';
+import React, { Component } from 'react';
+import {StyleSheet, View, Text, TextInput, Button, Pressable } from 'react-native';
 
-//$ npm install @react-native-picker/picker --save
+class CalculadoraCombustivel extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      precoLitro: '',
+      quantidadeAbastecida: '',
+      quilometrosPercorridos: '',
+      resultado: 0,
+    };
+  }
 
-export default function App() {
+  calcularCustoCombustivel = () => {
+    const { precoLitro, quantidadeAbastecida, quilometrosPercorridos } = this.state;
 
-  const [valorGasolina, setValorGasolina] = useState('');
-
-  const buscarHandle= async () =>{
-    let URL = `http://api.meuspostos.com.br/cidades.json`
-    try {
-      let page= await fetch(URL)
-      let json = await page.json()
-      console.log(json)  
-    } catch (error){
-      setValorConvertido(`Erro: ${error.message}`)
+    if (precoLitro && quantidadeAbastecida && quilometrosPercorridos) {
+      const custo = (precoLitro * quantidadeAbastecida) / quilometrosPercorridos;
+      this.setState({ resultado: custo.toFixed(2) });
     }
   };
 
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+         <View style={styles.content}>
+          <Text style={styles.texto}>Preço por Litro:</Text>
+          <TextInput
+            onChangeText={(precoLitro) => this.setState({ precoLitro })}
+            value={this.state.precoLitro}
+            keyboardType="numeric"
+            placeholder="Digite o preço por litro"
+          />
 
-  return (
-    <View style={styles.container}>
-      <Pressable onPress={buscarHandle}>
-              <Text style={styles.texto}>Buscar API no console</Text>
-          </Pressable>
-    </View>
-  );
+          <Text style={styles.texto}>Quantidade Abastecida (litros):</Text>
+          <TextInput
+            onChangeText={(quantidadeAbastecida) => this.setState({ quantidadeAbastecida })}
+            value={this.state.quantidadeAbastecida}
+            keyboardType="numeric"
+            placeholder="Digite a quantidade abastecida"
+          />
+
+          <Text style={styles.texto}>Quilômetros Percorridos:</Text>
+          <TextInput
+            onChangeText={(quilometrosPercorridos) => this.setState({ quilometrosPercorridos })}
+            value={this.state.quilometrosPercorridos}
+            keyboardType="numeric"
+            placeholder="Digite os quilômetros percorridos"
+          />
+
+          <Button title="Calcular" onPress={this.calcularCustoCombustivel} />
+
+          {this.state.resultado > 0 && (
+            <Text style={{ marginTop: 20 }}>
+              Custo por quilômetro: R$ {this.state.resultado}
+            </Text>
+          )}
+          
+          
+        </View>
+      </View>
+    );
+  }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000000',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  content: {
+    padding: 16,
+    backgroundColor:'#87CEFA',
+    boxShadow: 'none',
+    borderRadius: 10,
+    padding: 20,
+    margin: 20,
+  },
+  botao: {
+    backgroundColor: '#2ECC71',
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    padding: 10,
+  },
+  texto:{
+    fontSize: 20,
+    paddingTop: 10,
+    fontWeight: 'bold',
+  },
+
+})
+export default CalculadoraCombustivel;
